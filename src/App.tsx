b788fff7cc34efbaec7f89f82dcf1c9360d2b1fd
Lucas Henrique <lucasheriques@gmail.com>
2023-05-05 18:05:21 -0300

    adding convex infra

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import { useState } from "react";
 import AccordionDemo from "@/components/test";
+import { useQuery } from "../convex/_generated/react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const tasks = useQuery("getTasks");
 
   return (
-    <>
-      <h1 className="text-3xl font-bold">Hello world!</h1>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-      <AccordionDemo />
-      <p>
-        Edit <code>src/App.tsx</code> and save to test HMR
-      </p>
-    </>
+    <div className="bg-slate-900 text-white min-h-full">
+      <main className="container py-8">
+        <h1 className="text-3xl font-bold">Hello world!</h1>
+        {tasks?.map(({ _id, text }) => (
+          <div key={_id.toString()}>{text}</div>
+        ))}
+        <p></p>
+      </main>
+    </div>
   );
 }
 
